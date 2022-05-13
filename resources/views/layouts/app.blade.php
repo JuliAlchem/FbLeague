1ede6d3932a0f246f6cd2a31087eef68d778a570
JuliAlch <julialchem@gmail.com>
2022-05-13 14:33:45 +0200

    navBar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -8,17 +8,26 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
     <!-- Styles -->
-   
+    @livewireStyles
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+
+    @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
 </head>
 <body>
     
+    @livewire('navigation')
+    
 
-    @yield('content')
 
-
+    @livewireScripts
 <script>
 var sidebar = document.getElementById('sidebar');
 function sidebarToggle() {
@@ -37,5 +46,7 @@ function profileToggle() {
     }
 }
 </script>
+
+@livewireScripts
 </body>
 </html>
